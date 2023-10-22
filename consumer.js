82f99a3ec4062f6aaf331feebc6d52ba53ed3347
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2023-10-22 15:55:26 -0600

    asd

--- consumer.js
+++ consumer.js
@@ -20,11 +20,10 @@ async function consumeMessages() {
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       const parsedMessage = JSON.parse(message.value.toString());
-      const data = await axios.get(
-        `https://alertas-expedientes-api-production.up.railway.app/busqueda?fecha=${parsedMessage.fecha}&exp=${parsedMessage.exp}&extracto=${parsedMessage.extracto}&cve_juz=${parsedMessage.cve_juz}`
-      );
-        console.log(data.data);
-      console.log(`Recibido mensaje en ${topic}-${partition} | Offset: ${message.offset}, Valor: ${message.value}`);
+      console.log(parsedMessage);
+      // console.log(
+      //   `Recibido mensaje en ${topic}-${partition} | Offset: ${message.offset}, Valor: ${message.value}`
+      // );
     },
   });
 }
