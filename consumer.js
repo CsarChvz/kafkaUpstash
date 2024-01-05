456876bea45443350789c24cae2f606e03d578ca
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-01-05 10:14:04 -0600

    Update consumer.js

--- consumer.js
+++ consumer.js
@@ -2,11 +2,11 @@ const { Kafka } = require("kafkajs");
 const axios = require("axios");
 
 const kafka = new Kafka({
-  brokers: ["engaging-swan-10732-us1-kafka.upstash.io:9092"],
+  brokers: ["legible-duck-7093-us1-kafka.upstash.io:9092"],
   sasl: {
     mechanism: "scram-sha-256",
-    username: "ZW5nYWdpbmctc3dhbi0xMDczMiR4sCuIBVuISYn-VLSLYlosq_1_SK_gQQrC0Uc",
-    password: "ZjBjYjA4ODctZTBiMC00MmQ4LTk3NDItNTQwNWU2Y2QwMzQ4",
+    username: "bGVnaWJsZS1kdWNrLTcwOTMk2kYRHkeNGLe30E2DWXiXNOUnLvlZBqDzzR3wO9o",
+    password: "MmFiNGJkNDQtYTliMi00ZmQ4LWIxYmUtMjk0ODQxNjM2YjFl",
   },
   ssl: true,
 });
@@ -21,7 +21,7 @@ async function consumeMessages() {
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
         const parsedMessage = JSON.parse(message.value.toString());
-
+        console.log("Antes de actualizar", new Date());
         await axios.patch(
           `https://alertas-expedientes-api-production.up.railway.app/busqueda?fecha=${parsedMessage.fecha}&exp=${parsedMessage.exp}&extracto=${parsedMessage.extracto}&cve_juz=${parsedMessage.cve_juz}&idExpediente=${parsedMessage.idExpediente}`
         );
