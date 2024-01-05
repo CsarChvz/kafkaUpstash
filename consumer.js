5c538b3619290a99ad1b99714c805fc593301306
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2024-01-05 10:51:32 -0600

    xd

--- consumer.js
+++ consumer.js
@@ -21,11 +21,28 @@ async function consumeMessages() {
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
         const parsedMessage = JSON.parse(message.value.toString());
+        console.log("Mensaje recibido", parsedMessage);
         console.log("Antes de actualizar", new Date());
-        await axios.patch(
-          `http://localhost:3000/busqueda?fecha=${parsedMessage.fecha}&exp=${parsedMessage.exp}&extracto=${parsedMessage.extracto}&cve_juz=${parsedMessage.cve_juz}&idExpediente=${parsedMessage.idExpediente}`
-        );
+        // await axios.patch(
+        //   `http://localhost:3000/busqueda?fecha=${parsedMessage.fecha}&exp=${parsedMessage.exp}&extracto=${parsedMessage.extracto}&cve_juz=${parsedMessage.cve_juz}&idExpediente=${parsedMessage.idExpediente}`
+        // );
 
+        await axios
+          .patch(
+            `http://localhost:3000/busqueda?fecha=${parsedMessage.fecha}&exp=${parsedMessage.exp}&extracto=${parsedMessage.extracto}&cve_juz=${parsedMessage.cve_juz}&idExpediente=${parsedMessage.idExpediente}`
+          )
+          .then((response) => {
+            console.log("Response", response);
+          })
+          .catch((error) => {
+            console.log("Error", error);
+            if (!error.response) {
+              // network error
+              this.errorStatus = "Error: Network Error";
+            } else {
+              this.errorStatus = error.response.data.message;
+            }
+          });
         console.log("Actualizado", new Date());
       },
     });
