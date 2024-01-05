87f2070ac58ee15d9ef2677806ef0876d97bc40a
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2024-01-05 10:56:00 -0600

    xd

--- consumer.js
+++ consumer.js
@@ -21,10 +21,24 @@ async function consumeMessages() {
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
         const parsedMessage = JSON.parse(message.value.toString());
+        console.log("Message", parsedMessage);
         console.log("Antes de actualizar", new Date());
-        await axios.patch(
-          `https://alertas-expedientes-api-production.up.railway.app/busqueda?fecha=${parsedMessage.fecha}&exp=${parsedMessage.exp}&extracto=${parsedMessage.extracto}&cve_juz=${parsedMessage.cve_juz}&idExpediente=${parsedMessage.idExpediente}`
-        );
+        await axios
+          .patch(
+            `https://alertas-expedientes-api-production.up.railway.app/busqueda?fecha=${parsedMessage.fecha}&exp=${parsedMessage.exp}&extracto=${parsedMessage.extracto}&cve_juz=${parsedMessage.cve_juz}&idExpediente=${parsedMessage.idExpediente}`
+          )
+          .then((response) => {
+            console.log("Response", response);
+          })
+          .catch((error) => {
+            console.log("Error", error);
+            if (!error.response) {
+              // network error
+              this.errorStatus = "Error: Network Error";
+            } else {
+              this.errorStatus = error.response.data.message;
+            }
+          });
 
         console.log("Actualizado", new Date());
       },
